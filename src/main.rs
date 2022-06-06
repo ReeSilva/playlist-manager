e6797b866104a7d1b3862cb24d4345872bf518fe
Renato Biancalana da Silva <renato@reesilva.dev>
2022-06-06 20:57:10 +0000

    Showing button and begin of input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, Button, Entry, InputPurpose};
 
 fn main() {
     // Create a new application
@@ -15,10 +15,35 @@ fn main() {
 }
 
 fn build_ui(app: &Application) {
+    let username = Entry::builder()
+        .name("Username")
+        .has_frame(true)
+        .input_purpose(InputPurpose::Name)
+        .max_length(32)
+        .placeholder_text("Entry your username here")
+        .build();
+    
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label("Press me!")
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |button| {
+        let username_input = username.to_string();
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label(&username_input);
+    });
+    
     // Create a window and set the title
     let window = ApplicationWindow::builder()
         .application(app)
         .title("Playlist Manager")
+        .child(&button)
         .build();
 
     // Present Window
