1255c1e81ef6e8ca12a4b7532b47bd7f935f7611
Renato Biancalana da Silva <renato@reesilva.dev>
2022-06-06 16:54:57 +0200

    running on containers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow};
+
 fn main() {
-    println!("Hello, world!");
+    // Create a new application
+    let app = Application::builder()
+        .application_id("com.reesilva.playlist-manager")
+        .build();
+    
+    //Connect to "activate" signal of the `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a window and set the title
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Playlist Manager")
+        .build();
+
+    // Present Window
+    window.present();
 }
